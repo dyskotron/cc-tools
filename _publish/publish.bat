03f4c5f1bd18d5816be878257a4452ca9ac1088d
Matej Osanec <matejosanec@fb.com>
2025-02-20 15:04:12 +0100

    fix & simplify batch file for upload

--- _publish/publish.bat
+++ _publish/publish.bat
@@ -1,23 +1,7 @@
 @echo off
-setlocal
-
-:: Path to the virtual environment
-set VENV_PATH=venv
-
-:: Path to the Python upload script
-set UPLOAD_SCRIPT=upload_files.py
-
-:: Check if the virtual environment exists
-if not exist "%VENV_PATH%" (
-    echo Error: Virtual environment not found at %VENV_PATH%.
-    exit /b 1
-)
-
-:: Activate the virtual environment
-call "%VENV_PATH%\Scripts\activate.bat"
 
 :: Run the upload script
-python -u "%UPLOAD_SCRIPT%"
+call python -u "upload_files.py"
 
 :: Check if the script executed successfully
 if %errorlevel% == 0 (
@@ -26,8 +10,4 @@ if %errorlevel% == 0 (
     echo Error: Upload script encountered an issue.
 )
 
-:: Deactivate the virtual environment
-deactivate
-
-endlocal
 pause
